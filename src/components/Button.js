8df6098e383f08f568d8f4aa395b908455320e1e
David Burles <dburles@gmail.com>
2020-08-25 12:51:54 +1000

    parcel2 updates

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,33 +1,37 @@
-import { useModifiers } from 'mystical';
 import PropTypes from 'prop-types';
 
-const modifiers = {
-  default: {
-    padding: '2 3',
-    border: 'none',
-    borderRadius: '3px',
-    fontSize: 2,
-  },
-  variant: {
-    primary: {
-      // you can reference other values defined in the theme
-      color: 'white',
-      backgroundColor: 'primary',
-    },
-    secondary: {
-      color: 'text',
-      backgroundColor: 'yellow',
-    },
-  },
-};
-
-const Button = ({ variant = 'primary', ...props }) => {
-  const modifierStyle = useModifiers({ variant }, modifiers);
-  return <button {...props} css={modifierStyle} />;
+const Button = ({ children, color = 'blue', ...props }) => {
+  return (
+    <button
+      {...props}
+      css={{
+        padding: '3 4',
+        textAlign: 'left',
+        whiteSpace: 'nowrap',
+        borderWidth: 0,
+        borderRadius: '3',
+        color: `${color}.100`,
+        fontSize: 'base',
+        fontFamily: "'Inter', sans-serif",
+        fontWeight: 600,
+        backgroundColor: `${color}.500`,
+        boxShadow: 'md',
+        ':hover:not(:disabled)': {
+          backgroundColor: `${color}.400`,
+        },
+        ':disabled': {
+          opacity: 0.5,
+        },
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-  variant: PropTypes.oneOf(['primary', 'secondary']),
+  color: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
 
 export default Button;
